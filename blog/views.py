0c4ce620482251b63bd42000176b43e54fec214e
Hannah <nhannahsofia@gmail.com>
2023-08-09 10:50:02 -0300

    Inclui o model Post

--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,23 @@
 from django.shortcuts import render
 
-#Inclui a classe HttResponse
+# Inclui a classe HttResponse
 from django.http import HttpResponse
 
+from blog.models import Post
+
+# Define uma function view chamada index
 
 
-#Define uma function view chamada index
 def index(request):
-    #return HttpResponse('Olá Django - index')
+    # return HttpResponse('Olá Django - index')
     return render(request, 'index.html', {'titulo': 'Últimos Artigos'})
 
 
-
-#DEfine uma function chamada ola
-def ola(request):
-    #return HttpResponse('Ola Django')
-    return render(request, 'home.html')
+# DEfine uma function chamada ola
+def ola(request):  # Modificar
+    # return HttpResponse('Olá django')
+    posts = Post.objects.all()  # recupera todos os posts do banco de dados
+    context = {'posts_list': posts}  # cria um dicionário com os dado
+    # renderiza o template e passa o contexto
+    return render(request, 'posts.html', context)
 # Create your views here.
-
-
-
