3cab1206880bb42c0fe69bd7d84d3dc3422eba3a
nhannahminighini <nhannahsofia@gmail.com>
2023-09-06 10:34:17 -0300

    Respostas JSON para o model Post

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
 # nesta linha importamos as functions views
-from blog.views import index, ola, post_show, PostDetailView
+from blog.views import (
+    index, ola, post_show, PostDetailView, 
+    get_all_posts,
+    get_post )
 
 urlpatterns = [
     path('index/', index, name="index"),  # Define a rota / index
@@ -9,4 +12,6 @@ urlpatterns = [
     path('posts/all', ola, name="posts_list"),  # a view responsável é ola()
     path('post/<int:post_id>', post_show, name="exibe_post"),
     path('post/<int:pk>/show', PostDetailView.as_view(), name="post_detail"),
+    path('api/posts', get_all_posts, name="posts_data"),
+    path('api/posts/<int:post_id>', get_post, name="post_data")
 ]
