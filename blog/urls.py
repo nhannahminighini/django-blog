b8cc9b6422f5553112d524843680d250951b66a4
Hannah <nhannahsofia@gmail.com>
2023-09-13 11:02:05 -0300

    CREATE - CRUD Básico

--- blog/urls.py
+++ blog/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 
 # nesta linha importamos as functions views
 from blog.views import (
-    index, ola, post_show, PostDetailView, 
-    get_all_posts,
-    get_post )
+    index, ola, post_show, PostDetailView,
+    get_all_posts, get_post,
+    PostCreateView, create_post)
 
 urlpatterns = [
     path('index/', index, name="index"),  # Define a rota / index
@@ -13,5 +13,6 @@ urlpatterns = [
     path('post/<int:post_id>', post_show, name="exibe_post"),
     path('post/<int:pk>/show', PostDetailView.as_view(), name="post_detail"),
     path('api/posts', get_all_posts, name="posts_data"),
-    path('api/posts/<int:post_id>', get_post, name="post_data")
+    path('api/posts/<int:post_id>', get_post, name="post_data"),
+    path('post/add', PostCreateView.as_view(), name="post_add"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,9 @@ import json
 from django.core.serializers.json import DjangoJSONEncoder
 from django.views.generic.detail import DetailView
 from django.shortcuts import render, get_object_or_404
-
-# Inclui a classe HttResponse
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 
 from blog.models import Post
@@ -42,9 +43,11 @@ def get_all_posts(request):
     data = {'success': True, 'posts': posts}
     json_data = json.dumps(data, indent=1, cls=DjangoJSONEncoder)
     response = HttpResponse(json_data, content_type='application/json')
-    response['Access-Control-Allow-Origin'] = '*' # requisição de qualquer origem
-    
+    # requisição de qualquer origem
+    response['Access-Control-Allow-Origin'] = '*'
+
     return response
+
 
 def get_post(request, post_id):
     post = Post.objects.filter(
@@ -52,19 +55,54 @@ def get_post(request, post_id):
     ).values(
         'pk', 'body_text', 'pub_date'
     ).first()
-    
+
     data = {'success': True, 'post': post}
     status = 200
     if post is None:
         data = {'success': False, 'error': 'Post ID não existe.'}
-        status=404
-        
+        status = 404
+
     response = HttpResponse(
         json.dumps(data, indent=1, cls=DjangoJSONEncoder),
         content_type="application/json",
         status=status
     )
 
-    response['Access-Control-Allow-Origin'] = '*' # requisição de qualquer origem
+    # requisição de qualquer origem
+    response['Access-Control-Allow-Origin'] = '*'
 
     return response
+
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'post/post_form.html'
+    fields = ('body_text', )
+    success_url = reverse_lazy('posts_list')
+
+
+@csrf_exempt
+def create_post(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        body_text = data.get('body_text')
+        if body_text is None:
+            data = {'success': False, 'error': 'Texto do post inválido.'}
+            status = 400  # Bad Request => erro do client
+        else:
+            post = Post(body_text=body_text)
+            post.save()
+            post_data = Post.objects.filter(
+                pk=post.id
+            ).values(
+                'pk', 'body_text', 'pub_date'
+            ).first()
+            data = {'success': True, 'post': post_data}
+            status = 201  # Created
+        response = HttpResponse(
+            json.dumps(data, indent=1, cls=DjangoJSONEncoder),
+            content_type="application/json",
+            status=status
+        )
+        response['Access-Control-Allow-Origin'] = '*'
+        return response
